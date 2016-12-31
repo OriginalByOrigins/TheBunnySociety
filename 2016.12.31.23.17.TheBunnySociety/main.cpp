35fe3e8c63ccb02fe4d5e8e47459c8ffee44bb9e
Thai Ngoc <se7en@Thais-MacBook-Air.local>
2016-12-31 23:17:24 +0800

    Initial Commit

---
+++ 2016.12.31.23.17.TheBunnySociety/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  2016.12.31.23.17.TheBunnySociety
+//
+//  Created by Thai Cao Ngoc on 31/12/16.
+//  Copyright © 2016 Thai Cao Ngoc. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
